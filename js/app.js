cb83a43ce0f93479ff1f6e3b9049f7002f9009b5
DMitte <mittedany547@gmail.com>
2021-10-16 22:41:11 -0500

    terminado

--- js/app.js
+++ js/app.js
@@ -1,8 +1,17 @@
 $(document).ready(function () {
-    ancho= $(window).width();
-    $(document).on(function () {
-        if(ancho => 992){
-            $('.header-body.header').css('display','none');
+    $('#registro').validate({
+        /*reglas para la validacion*/
+        rules: {
+            email: {
+                required: true,
+                email: true
+            }
+        },
+        /*mensajes personalizados para al validacion*/
+        messages :{
+            email: {
+                email : 'Pleace enter a valid email address'
+            }
         }
     });
 });
